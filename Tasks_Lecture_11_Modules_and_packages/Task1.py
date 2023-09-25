a36728cd1e1d708ac7cd25d7112760be0d464170
Iliyan Kostadinov <iliyan.kostadinov@egt-digital.com>
2023-09-25 14:32:39 +0300

    Solution of Task1 from Lecture 11

---
+++ Tasks_Lecture_11_Modules_and_packages/Task1.py
@@ -0,0 +1,8 @@
+# Задача 1. Да се включи time module и да се извика функцията sleep. Накарайте
+# програмата да заспи за 5 секунди и тогава принтирайте “Time module imported”
+
+import time
+
+time.sleep(5)
+
+print("Time module imported")
